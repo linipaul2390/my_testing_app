0a095ad658830f9cd94733fda151f35f85463d50
linipaul2390 <linipaul23@gmail.com>
2018-01-30 23:24:44 -0800

    Image property in angular

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,5 @@
-var myApp = angular.module("myModule",[]);
-/*var MyController = function($scope){
-  $scope.message = "Hai how are you!!";
-};*/
-myApp.controller("MyController",function($scope){
+var myApp = angular.module("myModule",[])
+.controller("MyController",function($scope){
   var employee = {
     firstName:"Lini",
     lastName:"Paul",
@@ -14,3 +11,18 @@ myApp.controller("MyController",function($scope){
 
   $scope.employee = employee;
 });
+/*var MyController = function($scope){
+  $scope.message = "Hai how are you!!";
+};*/
+/*myApp.controller("MyController",function($scope){
+  var employee = {
+    firstName:"Lini",
+    lastName:"Paul",
+    gender:"Female",
+    country:"India",
+    state:"Kerala",
+    city:"Kannur"
+  };
+
+  $scope.employee = employee;
+});*/
